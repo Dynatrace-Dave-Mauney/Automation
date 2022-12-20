e6de7c9385fe083240ef0c8a4714168faa6ccc18
Dave Mauney <dave.mauney@dynatrace.com>
2022-12-20 13:19:39 -0500

    Added event reporting.

---
+++ Reporting/Events/report_events.py
@@ -0,0 +1,145 @@
+import datetime
+import os
+import requests
+
+
+def summarize(env, token):
+    return process(env, token, False)
+
+
+def process(env, token, print_mode):
+    summary = []
+
+    count_total = 0
+
+    endpoint = '/api/v2/events'
+    params = 'pageSize=1000'
+    events_json_list = get_rest_api_json(env, token, endpoint, params)
+    # print(events_json_list)
+
+    if print_mode:
+        print('Events')
+        print('id' + '|' + 'name')
+
+    for events_json in events_json_list:
+        inner_events_json_list = events_json.get('events')
+        for inner_events_json in inner_events_json_list:
+            # print(inner_events_json)
+            event_type = inner_events_json.get('eventType')
+            event_title = inner_events_json.get('title')
+            start_time = inner_events_json.get('startTime')
+            end_time = inner_events_json.get('endTime')
+            # print(f'{start_time} {end_time}')
+            start_date_time = convert_epoch_in_milliseconds_to_local(start_time)
+            end_date_time = convert_epoch_in_milliseconds_to_local(end_time)
+            formatted_duration = format_time_duration(start_time, end_time)
+
+            if print_mode:
+                print(f'{event_type}|{event_title}|{start_date_time}|{end_date_time}|{formatted_duration}')
+
+            count_total += 1
+
+    if print_mode:
+        print('Total events: ' + str(count_total))
+
+    summary.append('There are ' + str(count_total) + ' events currently defined.')
+
+    if print_mode:
+        print_list(summary)
+        print('Done!')
+
+    return summary
+
+
+def format_time_duration(start_time, end_time):
+    if end_time == -1:
+        return 'ONGOING'
+    else:
+        duration = (end_time - start_time) // 1000
+        millis = str(duration)[-3:]
+        days = hours = minutes = 0
+        if duration > 86400:
+            days = duration // 86400
+            duration -= days * 86400
+        if duration > 3600:
+            hours = duration // 3600
+            duration -= hours * 3600
+        if duration > 60:
+            minutes = duration // 60
+            duration -= minutes * 60
+        formatted_time_duration = f'{days}:{hours:02d}:{minutes:02d}:{duration:02d}.{millis}'
+        return formatted_time_duration
+
+
+def print_list(any_list):
+    for line in any_list:
+        line = line.replace('are 0', 'are no')
+        print(line)
+
+
+def get_rest_api_json(url, token, endpoint, params):
+    # print(f'get_rest_api_json({url}, {endpoint}, {params})')
+    full_url = url + endpoint
+    resp = requests.get(full_url, params=params, headers={'Authorization': "Api-Token " + token})
+    # print(f'GET {full_url} {resp.status_code} - {resp.reason}')
+    if resp.status_code != 200 and resp.status_code != 404:
+        print('REST API Call Failed!')
+        print(f'GET {full_url} {params} {resp.status_code} - {resp.reason}')
+        exit(1)
+
+    json_data = resp.json()
+
+    # Some json is just a list of dictionaries.
+    # Config V1 AWS Credentials is the only example I am aware of.
+    # For these, I have never seen pagination.
+    if type(json_data) is list:
+        # DEBUG:
+        # print(json_data)
+        return json_data
+
+    json_list = [json_data]
+    next_page_key = json_data.get('nextPageKey')
+
+    while next_page_key is not None:
+        # print(f'next_page_key: {next_page_key}')
+        params = {'nextPageKey': next_page_key}
+        full_url = url + endpoint
+        resp = requests.get(full_url, params=params, headers={'Authorization': "Api-Token " + token})
+        # print(resp.url)
+
+        if resp.status_code != 200:
+            print('Paginated REST API Call Failed!')
+            print(f'GET {full_url} {resp.status_code} - {resp.reason}')
+            exit(1)
+
+        json_data = resp.json()
+        # print(json_data)
+
+        next_page_key = json_data.get('nextPageKey')
+        json_list.append(json_data)
+
+    return json_list
+
+
+def convert_epoch_in_milliseconds_to_local(epoch):
+    if epoch == -1:
+        return None
+    else:
+        return datetime.datetime.fromtimestamp(epoch / 1000).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+
+
+def main():
+    # env_name, tenant_key, token_key = ('Prod', 'PROD_TENANT', 'ROBOT_ADMIN_PROD_TOKEN')
+    # env_name, tenant_key, token_key = ('Prep', 'PREP_TENANT', 'ROBOT_ADMIN_PREP_TOKEN')
+    env_name, tenant_key, token_key = ('Dev', 'DEV_TENANT', 'ROBOT_ADMIN_DEV_TOKEN')
+    # env_name, tenant_key, token_key = ('Personal', 'PERSONAL_TENANT', 'ROBOT_ADMIN_PERSONAL_TOKEN')
+
+    tenant = os.environ.get(tenant_key)
+    token = os.environ.get(token_key)
+    env = f'https://{tenant}.live.dynatrace.com'
+
+    process(env, token, True)
+
+
+if __name__ == '__main__':
+    main()
