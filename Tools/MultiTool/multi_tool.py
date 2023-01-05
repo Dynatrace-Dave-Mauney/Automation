ac56fdfe44bdf097865bf134916e00af51f985d8
Dave Mauney <dave.mauney@dynatrace.com>
2023-01-05 13:35:11 -0500

    Improved environment handling.

--- Tools/MultiTool/multi_tool.py
+++ Tools/MultiTool/multi_tool.py
@@ -324,7 +324,7 @@ def view_metric(metric_id, env, token):
             print(json.dumps(r.json(), indent=4))
         else:
             if r.status_code == 400 and "The requested metricId is invalid" in r.text:
-               print('Metric ID invalid')
+                print('Metric ID invalid')
             else:
                 if r.status_code == 404 and "No metric found using the Metric Id" in r.text:
                     print('Metric ID not found on this tenant')
@@ -353,7 +353,7 @@ def view_metric_query(metric_selector, env, token):
             print(json.dumps(r.json(), indent=4))
         else:
             if r.status_code == 400 and "The requested metricId is invalid" in r.text:
-               print('Metric ID invalid')
+                print('Metric ID invalid')
             else:
                 if r.status_code == 404 and "No metric found using the Metric Id" in r.text:
                     print('Metric ID not found on this tenant')
@@ -498,7 +498,31 @@ def change_environment(new_env):
         print('Invalid Environment Name...')
         return new_env, 'INVALID', 'NA'
 
-    return supported_environments.get(new_env)
+    return get_environment(new_env)
+
+
+def get_environment(env_name):
+    if env_name not in supported_environments:
+        print(f'Invalid environment name: {env_name}')
+        return env_name, None, None
+
+    env_name, tenant_key, token_key = supported_environments.get(env_name)
+
+    if env_name and tenant_key and token_key:
+        tenant = os.environ.get(tenant_key)
+        token = os.environ.get(token_key)
+        env = f'https://{tenant}.live.dynatrace.com'
+
+        if tenant and token and '.' in token:
+            masked_token = token.split('.')[0] + '.' + token.split('.')[1] + '.* (Masked)'
+            print(f'Environment Name: {env_name}')
+            print(f'Environment:      {env}')
+            print(f'Token:            {masked_token}')
+            return env_name, env, token
+        else:
+            print('Invalid Environment Configuration!')
+            print(f'Set the "env_name ({env_name}), tenant_key ({tenant_key}), token_key ({token_key})" tuple as required and verify the tenant ({tenant}) and token ({token}) environment variables are accessible.')
+            exit(1)
 
 
 def run():
@@ -507,20 +531,11 @@ def run():
     global save_id
     global save_content
 
-    # env_name, tenant_key, token_key = ('Prod', 'PROD_TENANT', 'ROBOT_ADMIN_PROD_TOKEN')
-    # env_name, tenant_key, token_key = ('Prep', 'PREP_TENANT', 'ROBOT_ADMIN_PREP_TOKEN')
-    env_name, tenant_key, token_key = ('Dev', 'DEV_TENANT', 'ROBOT_ADMIN_DEV_TOKEN')
-    # env_name, tenant_key, token_key = ('Personal', 'PERSONAL_TENANT', 'ROBOT_ADMIN_PERSONAL_TOKEN')
-
-    tenant = os.environ.get(tenant_key)
-    token = os.environ.get(token_key)
-    env = f'https://{tenant}.live.dynatrace.com'
-
-    masked_token = token.split('.')[0] + '.' + token.split('.')[1] + '.* (Masked)'
-
-    print(f'Environment Name: {env_name}')
-    print(f'Environment:      {env}')
-    print(f'Token:            {masked_token}')
+    # Set Environment
+    env_name, env, token = get_environment('Prod')
+    # env_name, env, token = get_environment('Prep')
+    # env_name, env, token = get_environment('Dev')
+    # env_name, env, token = get_environment('Personal')
 
     print_help()
 
@@ -645,26 +660,12 @@ def run():
             print(f'Environment is currently set to:')
             print(f'Environment Name: {env_name}')
             print(f'Environment:      {env}')
-            print(f'Token:            {masked_token}')
             continue
 
         if input_string.upper().startswith('E '):
             new_env = input_string.split(' ')[1]
 
-            if new_env not in supported_environments:
-                print('Invalid Environment Name...')
-                continue
-
-            env_name, tenant_key, token_key = change_environment(new_env)
-            tenant = os.environ.get(tenant_key)
-            token = os.environ.get(token_key)
-            env = f'https://{tenant}.live.dynatrace.com'
-
-            masked_token = token.split('.')[0] + '.' + token.split('.')[1] + '.* (Masked)'
-
-            print(f'Environment Name: {env_name}')
-            print(f'Environment:      {env}')
-            print(f'Token:            {masked_token}')
+            env_name, env, token = change_environment(new_env)
 
             continue
 
