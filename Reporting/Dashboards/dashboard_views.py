991d67aa1af317011259297f768c48f443ccbb91
Dave Mauney <dave.mauney@dynatrace.com>
2023-01-24 10:40:18 -0500

    Added double-check for missing dashboards and HTML and XLSX output.

--- Reporting/Dashboards/dashboard_views.py
+++ Reporting/Dashboards/dashboard_views.py
@@ -5,6 +5,7 @@
 import os
 import requests
 import urllib.parse
+import xlsxwriter
 
 
 from inspect import currentframe
@@ -71,7 +72,12 @@ def get_line_number():
 
 
 def process():
+    lines = []
+
     dashboard_details = load_dashboard_details()
+
+    # For testing double-check of get_dashboard
+    # dashboard_details = {}
 
     endpoint = '/api/v2/metrics/query'
     metric_schema_id = 'builtin:dashboards.viewCount'
@@ -91,9 +97,23 @@ def process():
                     dashboard_name = dashboard_detail.get('name', '')
                     dashboard_owner = dashboard_detail.get('owner', '')
                 else:
-                    dashboard_name = '*** DELETED ***'
-                    dashboard_owner = ''
-                print(dashboard_name + '|' + dashboard_id  + '|' + dashboard_owner + '|' + str(values))
+                    # If user is not in "dynatrace.com", then they will get this for all "dynatrace.com" owners.
+                    # So, we will double-check any missing ID by attempting to get it directly from the API
+                    dashboard = get_dashboard(dashboard_id)
+                    if dashboard:
+                        dashboard_name = dashboard.get('dashboardMetadata').get('name', '')
+                        dashboard_owner = dashboard.get('dashboardMetadata').get('owner', '')
+                    else:
+                        dashboard_name = '*** DELETED ***'
+                        dashboard_owner = ''
+
+                lines.append(f'{dashboard_name}|{dashboard_id}|{dashboard_owner}|{str(values)}')
+
+    for line in lines:
+        print(line)
+
+    write_xlsx(lines)
+    write_html(lines)
 
 
 def load_dashboard_details():
@@ -112,6 +132,116 @@ def load_dashboard_details():
             dashboard_details[entity_id] = {'name': name, 'owner': owner}
 
     return dashboard_details
+
+def get_dashboard(dashboard_id):
+    endpoint = f'/api/config/v1/dashboards/{dashboard_id}'
+    params = ''
+    dashboard_json_list = get_rest_api_json(env, token, endpoint, params)
+    if dashboard_json_list:
+        dashboard = dashboard_json_list[0]
+        if dashboard.get('dashboardMetadata'):
+            return dashboard
+
+    # Dashboard was not successfully retrieved
+    return None
+
+
+def write_xlsx(lines):
+    workbook = xlsxwriter.Workbook('../../$Output/Reporting/Dashboards/DashboardViews.xlsx')
+    header_format = workbook.add_format({'bold': True, 'bg_color': '#B7C9E2'})
+    number_format = workbook.add_format({'num_format': '0'})
+
+    worksheet = workbook.add_worksheet('Dashboard Views')
+
+    row_index = 0
+    # column_index = 0
+
+    headers = ['Dashboard Name', 'Dashboard ID', 'Owner', 'Views']
+    worksheet.write(row_index, 0, headers[0], header_format)
+    worksheet.write(row_index, 1, headers[1], header_format)
+    worksheet.write(row_index, 2, headers[2], header_format)
+    worksheet.write(row_index, 3, headers[3], header_format)
+    row_index += 1
+
+
+    for line in lines:
+        columns = line.split('|')
+        worksheet.write(row_index, 0, columns[0])
+        worksheet.write(row_index, 1, columns[1])
+        worksheet.write(row_index, 2, columns[2])
+        worksheet.write(row_index, 3, int(columns[3]), number_format)
+        row_index += 1
+
+    worksheet.autofilter(0, 2, row_index, 2) # add filter to only the third column (owner)
+    worksheet.autofit()
+    workbook.close()
+
+
+def write_html(lines):
+    filename = '../../$Output/Reporting/Dashboards/DashboardViews.html'
+
+    html_top = '''<html>
+      <body>
+        <head>
+          <style>
+            table, th, td {
+              border: 1px solid black;
+              border-collapse: collapse;
+            }
+            th, td {
+              padding: 5px;
+            }
+            th {
+              text-align: left;
+            }
+          </style>
+        </head>'''
+
+    table_header = '''    <table>
+          <tr>
+            <th>Dashboard Name</th>
+            <th>Dashboard ID</th>
+            <th>Owner</th>
+            <th>Views</th>
+          </tr>'''
+
+    html_bottom = '''    </table>
+      </body>
+    </html>'''
+
+    row_start = '<tr>'
+    row_end = '</tr>'
+    col_start = '<td>'
+    col_end = '</td>'
+
+    with open(filename, 'w', encoding='utf8') as file:
+        # Begin HTML formatting
+        write_line(file, html_top)
+
+        # Write the tag summary header
+        write_h1_heading(file, f'Dashboard Views')
+
+        # Write Table Header
+        write_line(file, table_header)
+
+        # Write Table Rows
+        for line in lines:
+            columns = line.split('|')
+            write_line(file, f'{row_start}{col_start}{columns[0]}{col_end}{col_start}{columns[1]}{col_end}{col_start}{columns[2]}{col_end}{col_start}{columns[3]}{col_end}{row_end}')
+
+        # Finish the HTML formatting
+        write_line(file, html_bottom)
+
+
+def write_h1_heading(outfile, heading):
+    outfile.write('    <h1>' + heading + '</h1>')
+    outfile.write('\n')
+
+
+def write_line(outfile, content):
+    outfile.write(content)
+    outfile.write('\n')
+
 
 if __name__ == '__main__':
     process()
