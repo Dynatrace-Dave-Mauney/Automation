3249c72676689511b4f8b89398ba4e3046f60b1b
Dave Mauney <dave.mauney@dynatrace.com>
2023-01-26 15:08:35 -0500

    update

--- Internal/SiteMapper/update_dynatrace_news_history.bat
+++ Internal/SiteMapper/update_dynatrace_news_history.bat
@@ -1,3 +1,7 @@
-py site_mapper.py
-git commit -a -m "update"
-git push
+cd C:\Users\Dave.Mauney\PycharmProjects\Automation\Internal\SiteMapper
+py site_mapper.py > log.txt
+cd C:\Users\Dave.Mauney\PycharmProjects\Automation\docs >> log.txt
+copy *.html C:\Users\Dave.Mauney\PycharmProjects\Automation\Internal\SiteMapper >> log.txt
+git status >> log.txt
+git commit -a -m "SiteMapper Scheduled Run" >> log.txt
+git push origin HEAD:main >> log.txt
