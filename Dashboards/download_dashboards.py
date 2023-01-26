b1293076a522fd58cca5818339513fffe0fcf85e
Dave Mauney <dave.mauney@dynatrace.com>
2023-01-26 15:04:32 -0500

    SiteMapper Scheduled Run

--- Dashboards/download_dashboards.py
+++ Dashboards/download_dashboards.py
@@ -1,11 +1,12 @@
 """
 Save dashboards from the tenant to the path indicated below.
 """
+import json
+import os
+import re
 import requests
 import ssl
-import os
 import sys
-import json
 
 
 def save(path, file, content):
@@ -19,11 +20,11 @@ def save_dashboards(env, token, path):
 	try:
 		headers = {'Authorization': 'Api-Token ' + token}
 		r = requests.get(env + '/api/config/v1/dashboards', headers=headers)
-		print("%s save list: %d" % ('dashboards', r.status_code))
+		# print("%s save list: %d" % ('dashboards', r.status_code))
 		# print(r.content)
 		res = r.json()
-		print(res)
-		print('Saving to directory path: ' + path)
+		# print(res)
+		# print('Saving to directory path: ' + path)
 		for entry in res['dashboards']:
 			dashboard_name = entry.get('name')
 			dashboard_id = entry.get('id')
@@ -32,14 +33,16 @@ def save_dashboards(env, token, path):
 			# if ((dashboard_name.startswith('Prod:') or dashboard_name.startswith('Prep:')) and dashboard_id.startswith('00000000-dddd-bbbb-ffff-0000000000')):
 			# if dashboard_owner == 'Dynatrace' and dashboard_name.startswith('A'):
 			# if dashboard_owner == 'nobody@example.com':
-			if dashboard_owner == 'Dynatrace':
-				print(dashboard_owner, dashboard_id, dashboard_name)
-				tr = requests.get(env + '/api/config/v1/dashboards/' + dashboard_id, headers=headers)
-				# save(path, dashboard_id, tr.json())
-				save(path, dashboard_name + '.json', tr.json())
+			# if dashboard_owner == 'Dynatrace':
+			if True:
+				response = requests.get(env + '/api/config/v1/dashboards/' + dashboard_id, headers=headers)
+				dashboard = response.json()
+				if 'ism74021' in str(dashboard):
+					clean_filename = re.sub(r"[/\\?%*:|\"<>\x7F\x00-\x1F]", "-", f'{dashboard_name}.json')
+					print(f'Saving {dashboard_name} ({dashboard_id}) owned by {dashboard_owner} to {clean_filename}')
+					save(path, clean_filename, dashboard)
 	except ssl.SSLError:
 		print("SSL Error")
-
 
 def main(arguments):
 	usage = '''
@@ -54,16 +57,16 @@ def main(arguments):
 	print('args' + str(arguments))
 	print(os.getcwd())
 
-	# env_name, tenant_key, token_key = ('Prod', 'PROD_TENANT', 'ROBOT_ADMIN_PROD_TOKEN')
+	env_name, tenant_key, token_key = ('Prod', 'PROD_TENANT', 'ROBOT_ADMIN_PROD_TOKEN')
 	# env_name, tenant_key, token_key = ('Prep', 'PREP_TENANT', 'ROBOT_ADMIN_PREP_TOKEN')
 	# env_name, tenant_key, token_key = ('Dev', 'DEV_TENANT', 'ROBOT_ADMIN_DEV_TOKEN')
-	env_name, tenant_key, token_key = ('Personal', 'PERSONAL_TENANT', 'ROBOT_ADMIN_PERSONAL_TOKEN')
+	# env_name, tenant_key, token_key = ('Personal', 'PERSONAL_TENANT', 'ROBOT_ADMIN_PERSONAL_TOKEN')
 
 	tenant = os.environ.get(tenant_key)
 	token = os.environ.get(token_key)
 	env = f'https://{tenant}.live.dynatrace.com'
 
-	path = '../$Output/Dashboards/Downloads'
+	path = f'../$Output/Dashboards/Downloads/{env_name}'
 
 	if len(arguments) == 1:
 		save_dashboards(env, token, path)
