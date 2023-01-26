462386704303143f031564b05dbca0e4a92b1421
Dave Mauney <dave.mauney@dynatrace.com>
2023-01-26 16:00:15 -0500

    SiteMapper Automation.

--- Internal/SiteMapper/update_dynatrace_news_history.bat
+++ Internal/SiteMapper/update_dynatrace_news_history.bat
@@ -1,5 +1,6 @@
 cd C:\src\github\Automation\Internal\SiteMapper
-git fetch > C:\src\github\Automation\Internal\SiteMapper\log.txt
+git fetch --all > C:\src\github\Automation\Internal\SiteMapper\log.txt
+git reset --hard origin/main >> C:\src\github\Automation\Internal\SiteMapper\log.txt
 py site_mapper.py >> C:\src\github\Automation\Internal\SiteMapper\log.txt
 cd C:\src\github\Automation\docs >> C:\src\github\Automation\Internal\SiteMapper\log.txt
 copy dynatrace*.html C:\src\github\Automation\Internal\SiteMapper >> C:\src\github\Automation\Internal\SiteMapper\log.txt
