f1421937443b4deaea2a48dd324a603b349ab343
Dave Mauney <dave.mauney@dynatrace.com>
2023-03-13 14:37:49 -0400

    Minor improvements.

--- Dashboards/create_markdown_menus.py
+++ Dashboards/create_markdown_menus.py
@@ -24,26 +24,7 @@ def write_markdown_menus(dashboard_lookup):
     menu_item_list = [
         '.NET',
         'Administration',
-        'AWS ALB',
-        'AWS API Gateway',
-        'AWS CloudWatch Logs',
-        'AWS Cloudfront',
-        'AWS Connect',
-        'AWS DynamoDB',
-        'AWS EBS',
-        'AWS EC2',
-        'AWS ECS ContainerInsights',
-        'AWS ECS',
-        'AWS ES',
-        'AWS Lambda Functions',
-        'AWS Lex',
-        'AWS NAT Gateways',
-        'AWS NLB',
-        'AWS RDS',
-        'AWS Route 53 Resolver',
-        'AWS Route 53',
-        'AWS Site-to-Site VPN',
-        'AWS SQS',
+        'AWS Home',
         'Application Overview (HTTP Monitors and Services)',
         'Application Overview (Services)',
         'Application Overview (Synthetics and Services)',
@@ -88,21 +69,6 @@ def write_markdown_menus(dashboard_lookup):
     filename = 'Templates/Overview/markdown_menu.json'
     with open(filename, 'w') as file:
         file.write(markdown_menu)
-
-    aws_markdown_menu = '			"markdown": "'
-    for menu_item in menu_item_list:
-        if 'AWS' in menu_item:
-            aws_markdown_item_id = dashboard_lookup.get(menu_item)
-            if not aws_markdown_item_id:
-                print(f'Dashboard lookup dictionary missing menu item: {menu_item}')
-            aws_markdown_menu += f'[{menu_item}](#dashboard;id={aws_markdown_item_id})  \\n'
-    markdown_menu += '"'
-
-    filename = 'Templates/Overview/markdown_aws_menu.json'
-    with open(filename, 'w') as file:
-        file.write(aws_markdown_menu)
-
-
 
 def main():
     dashboard_lookup = load_dashboard_lookup()
