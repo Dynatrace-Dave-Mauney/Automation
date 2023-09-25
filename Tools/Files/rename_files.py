a2428cebdc573ef0b4b71dd877af4d04e1de1f22
dave.mauney <dave.mauney@dynatrace.com>
2023-09-25 09:28:00 -0400

    Tweaks

--- Tools/Files/rename_files.py
+++ Tools/Files/rename_files.py
@@ -7,12 +7,13 @@ import os
 
 def main():
     try:
-        directory_name = "../../Dashboards/Templates/Overview"
+        directory_name = "../../NewPlatform/Dashboards"
         for file_name in os.listdir(directory_name):
-            if os.path.isfile(f'{directory_name}/{file_name}') and file_name.startswith('00000000-dddd-bbbb-ffff') and not file_name.endswith('.json'):
+            if os.path.isfile(f'{directory_name}/{file_name}') and '- ' in file_name and file_name.endswith('.json'):
                 print(f'Renaming {directory_name}/{file_name}')
+                new_file_name = file_name.replace('- ', ' - ')
                 src = f'{directory_name}/{file_name}'
-                dst = f'{directory_name}/{file_name}.json'
+                dst = f'{directory_name}/{new_file_name}'
                 os.rename(src, dst)
     except FileNotFoundError:
         print('The directory name does not exist')
