09a6460b783d62e61c2d2e2889299899b40a81ba
dave.mauney <dave.mauney@dynatrace.com>
2023-10-09 10:52:47 -0400

    Tweaks

--- Tools/Files/generate_automation_report_list_for_pipeline_yaml.py
+++ Tools/Files/generate_automation_report_list_for_pipeline_yaml.py
@@ -1,5 +1,5 @@
 import glob
-import os.path
+# import os.path
 
 ignore_list = [
     'dynatrace_rest_api_helper.py',
@@ -7,14 +7,26 @@ ignore_list = [
     'perform_report_details_testing.py',
     'perform_summarize_environment_html.py',
     'perform_summarize_environment_print.py',
+    'report_activegate_certificate_details.py',
+    'report_activegate_oneagent_communication_comparison_details.py',
+    'report_aws_credential_details.py',
+    'report_aws_credential_details_specific_customer.py',
+    'report_aws_supporting_service_details.py',
+    'report_deployment_api_details.py',
+    'report_host_group_details_customer_specific.py',
+    'AWS/EC2/report_aws_ec2_hosts.py',
+    'Dashboards/mda_dashboard_reference_check.py',
+    'DeploymentAPI/report_deployment_api_details.py',
 ]
+
 
 def find_matching_py_files():
     for filename in glob.glob('../../Reporting/**/*.py', recursive=True):
-        with open(filename, 'r', encoding='utf-8') as f:
+        with open(filename, 'r', encoding='utf-8'):
             report_name = filename.replace('../../Reporting\\', '').replace('\\', '/')
             if report_name not in ignore_list:
                 print(f'  - {report_name}')
+
 
 def main():
     find_matching_py_files()
