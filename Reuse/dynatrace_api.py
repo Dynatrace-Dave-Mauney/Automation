dd43d5d030ce9df98aa8e4fde673c3427c5d3cb9
dave.mauney <dave.mauney@dynatrace.com>
2023-11-21 17:13:20 -0500

    Refactor for exponential backoff retries, rate limiting, and exception handling.

--- Reuse/dynatrace_api.py
+++ Reuse/dynatrace_api.py
@@ -1,159 +1,128 @@
-import os
 import json
+import os
+import random
 import requests
-import ssl
 import time
+import traceback
 
-from inspect import currentframe
+from functools import partial
 from json import JSONDecodeError
-from requests import Response
+
+
+class RateLimitException(Exception):
+    """Raised when HTTP Status Code 429 indicates rate limiting"""
+    pass
+
 
 # For get only currently, support bypassing cert verification
 verify_certificate = os.getenv('DYNATRACE_API_VERIFY_CERTIFICATE', 'true')
 if verify_certificate.lower() == 'false':
     verify_certificate = False
     print('WARNING: Certificate verification is being bypassed (for gets only)')
+    requests.packages.urllib3.disable_warnings()
 else:
     verify_certificate = True
 
 
 def get(url, token, endpoint, params):
-    # token_string = token.split('.')[0] + '.' + token.split('.')[1]
-    # print(token_string)
-    # print(f'get({url}, {endpoint}, {params})')
-    # Allow for rare cases of passing the complete endpoint as a URL, or the much more common case
-    # of just passing the relative path of the endpoint
-    # For the very special case of calling ActiveGate endpoints over port 9999, do not validate certificate
-    # Also, Bypass certificate verification if appropriate environment variable is set
+    # For debugging pagination
+    # page_number = 1
+
+    # Allow for rare cases of passing the complete endpoint as a URL in addition to the common case of passing the relative path of the endpoint
+    # For the very special (and untested) case of calling ActiveGate endpoints over port 9999, do not verify the certificate
     verify = verify_certificate
+
     if endpoint.startswith('https://'):
         full_url = endpoint
         if ':9999' in endpoint:
             verify = False
+            requests.packages.urllib3.disable_warnings()
     else:
         full_url = url + endpoint
 
-    if not verify:
-        requests.packages.urllib3.disable_warnings()
+    fn = partial(rate_limited_get, full_url, params=params, headers={'Authorization': "Api-Token " + token}, timeout=60.0, verify=verify)
+    try:
+        r = retry_with_backoff(fn)
+        r.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        handle_exception(e, f'GET Failed for URL "{full_url}" with parameters "{params}"')
+        raise SystemExit(e)
 
     try:
-        resp = requests.get(full_url, params=params, headers={'Authorization': "Api-Token " + token}, timeout=60.0, verify=verify)
-    except (ConnectionError, TimeoutError):
-        print('Sleeping 30 seconds before retrying due to connection or timeout error...')
-        time.sleep(30)
-        resp = requests.get(full_url, params=params, headers={'Authorization': "Api-Token " + token})
+        json_data = r.json()
+    except JSONDecodeError as e:
+        handle_exception(e, f'JSON decode error. Response Text: {r.text}')
+        raise SystemExit(e)
 
-    # print(f'GET {full_url} {resp.status_code} - {resp.reason}')
-    if resp.status_code != 200 and resp.status_code != 404:
-        print('REST API Call Failed!')
-        print(f'GET {full_url} {params} {resp.status_code} - {resp.reason}')
-        exit(1)
+    # Some json is just a list of dictionaries.
+    # Config V1 AWS Credentials is the only example I am aware of.
+    # For these, I have never seen pagination.
+    if type(json_data) is list:
+        return json_data
 
-    try:
-        json_data = resp.json()
+    json_list = [json_data]
+    next_page_key = json_data.get('nextPageKey')
 
-        # Some json is just a list of dictionaries.
-        # Config V1 AWS Credentials is the only example I am aware of.
-        # For these, I have never seen pagination.
-        if type(json_data) is list:
-            # DEBUG:
-            # print(json_data)
-            return json_data
+    while next_page_key is not None:
+        # For debugging pagination
+        # page_number += 1
+        # print(f'Getting page number {page_number} with next page key of "{next_page_key}"')
+        params = {'nextPageKey': next_page_key}
+        full_url = url + endpoint
+        fn = partial(rate_limited_get, full_url, params=params, headers={'Authorization': "Api-Token " + token}, timeout=60.0, verify=verify)
+        try:
+            r = retry_with_backoff(fn)
+            r.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            handle_exception(e, f'Paginated GET Failed with {r.status_code} - {r.reason} {r.text}. URL: {full_url}. Params: {params}')
+            raise SystemExit(e)
 
-        json_list = [json_data]
+        json_data = r.json()
+
         next_page_key = json_data.get('nextPageKey')
+        json_list.append(json_data)
 
-        while next_page_key is not None:
-            # print(f'next_page_key: {next_page_key}')
-            params = {'nextPageKey': next_page_key}
-            full_url = url + endpoint
-            resp = requests.get(full_url, params=params, headers={'Authorization': "Api-Token " + token}, timeout=60.0, verify=verify)
-            # print(resp.url)
-
-            if resp.status_code != 200:
-                print('Paginated REST API Call Failed!')
-                print(f'GET {full_url} {resp.status_code} - {resp.reason}')
-                exit(1)
-
-            json_data = resp.json()
-            # print(json_data)
-
-            next_page_key = json_data.get('nextPageKey')
-            json_list.append(json_data)
-
-        return json_list
-
-    except JSONDecodeError:
-        print('JSON decode error. Response: ')
-        print(resp)
-        print(resp.text)
-        exit(1)
+    return json_list
 
 
 def get_object_list(env, token, endpoint):
     url = env + endpoint
-    try:
-        r = requests.get(url, params='', headers={'Authorization': 'Api-Token ' + token})
-        if r.status_code not in [200]:
-            print(r.status_code)
-            print(r.reason)
-            exit(get_line_number())
-        return r
-    except ssl.SSLError:
-        print('SSL Error')
-        exit(get_line_number())
+    return get_without_pagination(url, token)
 
 
 def get_plain_text_list(env, token, endpoint):
     url = env + endpoint
-    try:
-        r = requests.get(url, params='', headers={'Authorization': 'Api-Token ' + token})
-        if r.status_code not in [200]:
-            print(r.status_code)
-            print(r.reason)
-            exit(get_line_number())
-        return r.text
-    except ssl.SSLError:
-        print('SSL Error')
-        exit(get_line_number())
+    return get_without_pagination(url, token)
 
 
 def get_by_object_id(env, token, endpoint, object_id):
     url = env + endpoint + '/' + object_id
+    r = get_without_pagination(url, token)
+    return json.loads(r.text)
+
+
+def get_without_pagination(url, token):
+    """get with rate limit and connection exception exponential backoff, but without pagination support"""
+    fn = partial(rate_limited_get, url, params='', headers={'Authorization': "Api-Token " + token}, timeout=60.0, verify=verify_certificate)
     try:
-        r = requests.get(url, params='', headers={'Authorization': 'Api-Token ' + token})
-        if r.status_code not in [200]:
-            print('Error in "dynatrace_api.get_by_object_id(env, token, endpoint, object_id)" method')
-            print('Endpoint: ' + endpoint)
-            print('Object ID: ' + object_id)
-            print('Exit code shown below is the source code line number of the exit statement invoked')
-            exit(get_line_number())
-        return json.loads(r.text)
-    except ssl.SSLError:
-        print('SSL Error')
-        exit(get_line_number())
+        r = retry_with_backoff(fn)
+        r.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        handle_exception(e, f'Failed to get url "{url}"')
+        raise SystemExit(e)
+
+    return r
 
 
-def post(env, token, endpoint: str, payload: str) -> Response:
+def post(env, token, endpoint, payload):
     # In general, avoid post in favor of put so "fixed ids" can be used
     json_data = json.loads(payload)
-    # print(payload)
-    # Remove id if present
-    # print(f'Popped: {json_data.pop("id")}')
-    # json_data.pop("id")
     formatted_payload = json.dumps(json_data, indent=4, sort_keys=False)
     url = env + endpoint
+    fn = partial(rate_limited_post, url, formatted_payload.encode('utf-8'), headers={'Authorization': 'Api-Token ' + token, 'Content-Type': 'application/json; charset=utf-8'}, timeout=60.0, verify=verify_certificate)
     try:
-        r: Response = requests.post(url, formatted_payload.encode('utf-8'), headers={'Authorization': 'Api-Token ' + token, 'Content-Type': 'application/json; charset=utf-8'})
-        # print('Status Code: %d' % r.status_code)
-        # print('Reason: %s' % r.reason)
-        # if len(r.text) > 0:
-        #     print(r.text)
+        r = retry_with_backoff(fn)
         if r.status_code not in [200, 201, 204]:
-            print('Status Code: %d' % r.status_code)
-            print('Reason: %s' % r.reason)
-            if len(r.text) > 0:
-                print(r.text)
             error_filename = '$post_error_payload.json'
             with open(error_filename, 'w') as file:
                 file.write(formatted_payload)
@@ -164,97 +133,126 @@ def post(env, token, endpoint: str, payload: str) -> Response:
                 except AttributeError:
                     print(formatted_payload)
                 print('Error in "dynatrace_api.post(env, token, endpoint: str, payload: str)" method')
-                print('Exit code shown below is the source code line number of the exit statement invoked')
                 print('See ' + error_filename + ' for more details')
-            exit(get_line_number())
-        return r
-    except ssl.SSLError:
-        print('SSL Error')
-        exit(get_line_number())
+        r.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        handle_exception(e, f'Failed to put to endpoint "{endpoint}" for payload "{payload}"')
+        raise SystemExit(e)
+
+    return r
+
+
+def post_plain_text(env, token, endpoint, payload):
+    url = env + endpoint
+
+    fn = partial(rate_limited_post, url, payload, headers={'Authorization': 'Api-Token ' + token, 'accept':  '*/*', 'Content-Type': 'text/plain; charset=utf-8'}, timeout=60.0, verify=verify_certificate)
+    try:
+        r = retry_with_backoff(fn)
+        r.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        handle_exception(e, f'Failed to post plain text to endpoint "{endpoint}" for payload "{payload}"')
+        raise SystemExit(e)
+
+    return r
 
 
 def put(env, token, endpoint, object_id, payload):
     # In general, favor put over post so "fixed ids" can be used
     json_data = json.dumps(json.loads(payload), indent=4, sort_keys=False)
-    url = env + endpoint + '/' + object_id
+    if object_id:
+        url = env + endpoint + '/' + object_id
+    else:
+        url = env + endpoint
+
+    fn = partial(rate_limited_put, url, json_data.encode('utf-8'), headers={'Authorization': 'Api-Token ' + token, 'Content-Type': 'application/json; charset=utf-8'}, timeout=60.0, verify=verify_certificate)
     try:
-        r: Response = requests.put(url, json_data.encode('utf-8'), headers={'Authorization': 'Api-Token ' + token, 'Content-Type': 'application/json; charset=utf-8'})
+        r = retry_with_backoff(fn)
         if r.status_code not in [200, 201, 204]:
-            print('Status Code: %d' % r.status_code)
-            print('Reason: %s' % r.reason)
-            if len(r.text) > 0:
-                print(r.text)
             error_filename = '$put_error_payload.json'
             with open(error_filename, 'w') as file:
                 file.write(json_data)
                 print('Error in "dynatrace_api.put(env, token, endpoint, object_id, payload)" method')
-                print('Exit code shown below is the source code line number of the exit statement invoked')
                 print('See ' + error_filename + ' for more details')
-            exit(get_line_number())
-        return r
-    except ssl.SSLError:
-        print('SSL Error')
-        exit(get_line_number())
+        r.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        handle_exception(e, f'Failed to put to endpoint "{endpoint}" for payload "{payload}"')
+        raise SystemExit(e)
+
+    return r
 
 
 def put_without_id(env, token, endpoint, payload):
-    json_data = json.dumps(json.loads(payload), indent=4, sort_keys=False)
-    url = env + endpoint
-    try:
-        r: Response = requests.put(url, json_data.encode('utf-8'), headers={'Authorization': 'Api-Token ' + token, 'Content-Type': 'application/json; charset=utf-8'})
-        if r.status_code not in [200, 201, 204]:
-            error_filename = '$put_error_payload.json'
-            with open(error_filename, 'w') as file:
-                file.write(json_data)
-                print('Error in "dynatrace_api.put(env, token, endpoint, object_id, payload)" method')
-                print('Exit code shown below is the source code line number of the exit statement invoked')
-                print('See ' + error_filename + ' for more details')
-            exit(get_line_number())
-        return r
-    except ssl.SSLError:
-        print('SSL Error')
-        exit(get_line_number())
-
-
-def get_line_number():
-    cf = currentframe()
-    return cf.f_back.f_lineno
-
-
-def post_plain_text(env, token, endpoint, payload):
-    url = env + endpoint
-    print('POST: ' + url)
-    print('payload: ' + payload)
-    try:
-        r = requests.post(url, payload, headers={'Authorization': 'Api-Token ' + token, 'accept':  '*/*', 'Content-Type': 'text/plain; charset=utf-8'})
-        print('Status Code: %d' % r.status_code)
-        print('Reason: %s' % r.reason)
-        if len(r.text) > 0:
-            print(r.text)
-        if r.status_code not in [200, 201, 202, 204]:
-            exit()
-    except ssl.SSLError:
-        print('SSL Error')
+    return put(env, token, endpoint, None, payload)
 
 
 def delete(env, token, endpoint, object_id):
     url = f'{env}{endpoint}/{object_id}'
+
+    fn = partial(rate_limited_delete, url, headers={'Authorization': "Api-Token " + token}, timeout=60.0, verify=verify_certificate)
     try:
-        r = requests.delete(url, headers={'Authorization': 'Api-Token ' + token, 'Content-Type': 'application/json; charset=utf-8'})
-        if r.status_code not in [200, 201, 204]:
-            print('Status Code: %d' % r.status_code)
-            print('Reason: %s' % r.reason)
-            if len(r.text) > 0:
-                print(r.text)
-            exit()
-    except ssl.SSLError:
-        print('SSL Error')
+        r = retry_with_backoff(fn)
+        r.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        handle_exception(e, f'Failed to delete object id "{object_id}" from endpoint "{endpoint}"')
+        raise SystemExit(e)
+
+    return r
 
 
-def delete_with_response(env, token, endpoint, object_id):
-    url = f'{env}{endpoint}/{object_id}'
-    try:
-        r = requests.delete(url, headers={'Authorization': 'Api-Token ' + token, 'Content-Type': 'application/json; charset=utf-8'})
-        return r
-    except ssl.SSLError:
-        print('SSL Error')
+def rate_limited_get(url, params, headers, timeout, verify):
+    r = requests.get(url, params, headers=headers, timeout=timeout, verify=verify)
+    if r.status_code == 429:
+        print('Rate limiting in effect.  Performing exponential backoff retries.')
+        raise RateLimitException
+    return r
+
+
+def rate_limited_delete(url, headers, timeout, verify):
+    r = requests.delete(url, headers=headers, timeout=timeout, verify=verify)
+    if r.status_code == 429:
+        print('Rate limiting in effect.  Performing exponential backoff retries.')
+        raise RateLimitException
+    return r
+
+
+def rate_limited_post(url, payload, headers, timeout, verify):
+    r = requests.post(url, payload, headers=headers, timeout=timeout, verify=verify)
+    if r.status_code == 429:
+        print('Rate limiting in effect.  Performing exponential backoff retries.')
+        raise RateLimitException
+    return r
+
+
+def rate_limited_put(url, payload, headers, timeout, verify):
+    r = requests.put(url, payload, headers=headers, timeout=timeout, verify=verify)
+    if r.status_code == 429:
+        print('Rate limiting in effect.  Performing exponential backoff retries.')
+        raise RateLimitException
+    return r
+
+
+def retry_with_backoff(fn, retries=5, backoff_in_seconds=1):
+    """Simple exponential backoff implementation from https://keestalkstech.com/2021/03/python-utility-function-retry-with-exponential-backoff/
+    with minor modifications (specify exceptions handled, modified formatting)"""
+    x = 0
+    while True:
+        try:
+            return fn()
+        except (ConnectionError, TimeoutError, RateLimitException):
+            if x == retries:
+                raise
+
+            sleep = (backoff_in_seconds * 2 ** x + random.uniform(0, 1))
+            time.sleep(sleep)
+            x += 1
+
+
+# Wrapper for requests that converts HTTP Status Code 429 to RateLimitException
+def handle_exception(e, message):
+    print(message)
+    if isinstance(e, requests.exceptions.RequestException):
+        if e.response.text:
+            print("Please check the following HTTP response to troubleshoot the issue:")
+            print(e.response.text)
+    print("Exiting Program")
+    traceback.print_exc()
--- Test/Reuse/test_dynatrace_api.py
+++ Test/Reuse/test_dynatrace_api.py
@@ -5,82 +5,147 @@ from Reuse import dynatrace_api
 from Reuse import environment
 
 
+def test_get_object_list(env, token, endpoint):
+    r = dynatrace_api.get_object_list(env, token, endpoint)
+    return r
+
+
 def test_get_alerting_profiles(env, token):
     endpoint = '/api/config/v1/alertingProfiles'
     params = ''
-    results = dynatrace_api.get(env, token, endpoint, params)
-    print(results)
+    r = dynatrace_api.get(env, token, endpoint, params)
+    return r
 
 
 def test_get_metrics(env, token):
     endpoint = '/api/v2/metrics'
     raw_params = 'pageSize=1000&fields=+displayName,+description,+unit,+aggregationTypes,+defaultAggregation,+dimensionDefinitions,+transformations,+entityType'
     params = urllib.parse.quote(raw_params, safe='/,&=')
-    results = dynatrace_api.get(env, token, endpoint, params)
-    print(results)
+    r = dynatrace_api.get(env, token, endpoint, params)
+    return r
 
 
 def test_get_entity_types(env, token):
     endpoint = '/api/v2/entityTypes'
     raw_params = 'pageSize=500'
     params = urllib.parse.quote(raw_params, safe='/,&=')
-    results = dynatrace_api.get(env, token, endpoint, params)
-    print(results)
+    r = dynatrace_api.get(env, token, endpoint, params)
+    return r
 
 
 def test_get_entity_type_host(env, token):
     endpoint = '/api/v2/entities'
     raw_params = 'pageSize=500&entitySelector=type(HOST)&fields=+properties.monitoringMode, +properties.state,+toRelationships'
     params = urllib.parse.quote(raw_params, safe='/,&=')
-    hosts = dynatrace_api.get(env, token, endpoint, params)
-    print(hosts)
-    for entities in hosts:
-        total_count = int(entities.get('totalCount'))
-        if total_count > 0:
-            host_entities = entities.get('entities')
-            for host in host_entities:
-                print(host)
+    r = dynatrace_api.get(env, token, endpoint, params)
+    return r
 
 
-def test_post(env, token):
-    endpoint = '/api/config/v1/autoTags'
-    payload = json.dumps({'name': 'TestAutoTag', 'rules': []})
-    dynatrace_api.post(env, token, endpoint, payload)
+def test_post(env, token, endpoint, payload):
+    r = dynatrace_api.post(env, token, endpoint, payload)
+    return r
 
 
-
-def test_post_plain_text(env, token):
-    endpoint = '/api/v2/metrics/ingest'
-    payload = 'com.dynatrace.automation.responseTime,language=python 1000'
-    dynatrace_api.post_plain_text(env, token, endpoint, payload)
+def test_post_plain_text(env, token, endpoint, payload):
+    r = dynatrace_api.post_plain_text(env, token, endpoint, payload)
+    return r
 
 
-def test_put(env, token):
-    endpoint = '/api/config/v1/autoTags'
-    object_id = 'ab3162ba-71d5-3ceb-b92d-408656024be8'
-    payload = json.dumps({'name': 'TestAutoTag', 'rules': []})
-    dynatrace_api.put(env, token, endpoint, object_id, payload)
+def test_put(env, token, endpoint, object_id, payload):
+    r = dynatrace_api.put(env, token, endpoint, object_id, payload)
+    return r
 
 
-def test_delete(env, token):
-    endpoint = '/api/config/v1/autoTags'
-    object_id = 'ab3162ba-71d5-3ceb-b92d-408656024be8'
+def test_delete(env, token, endpoint, object_id):
     dynatrace_api.delete(env, token, endpoint, object_id)
 
 
 def main():
     _, env, token = environment.get_environment('Personal')
-    # test_get_alerting_profiles(env, token)
-    # test_get_metrics(env, token)
-    # test_get_entity_types(env, token)
-    # test_get_entity_type_host(env, token)
 
-    # test_post(env, token)
-    # test_post_plain_text(env, token)
-    # test_put(env, token)
-    # test_delete(env, token)
+    endpoint = '/api/config/v1/autoTags'
+    r = test_get_object_list(env, token, endpoint)
+    # object_json_list = json.loads(r.text).get('values')
+    if r.status_code != 200:
+        print(f'Unexpected status code {r.status_code} from "test_get_object_list" method for endpoint "{endpoint}"')
+        print_response_details(r)
+        exit(1)
+
+    # print(f'Result successfully obtained for endpoint "{endpoint}" from "test_get_object_list" method: {object_json_list}')
+    print(f'Result successfully obtained for endpoint "{endpoint}" from "test_get_object_list" method')
+
+    alerting_profiles_list = test_get_alerting_profiles(env, token)
+    # print(f'Result successfully obtained from "test_get_alerting_profiles" method: {alerting_profiles_list}')
+    print(f'Result successfully obtained from "test_get_alerting_profiles" method')
+
+    metrics_list = test_get_metrics(env, token)
+    # print(f'Result successfully obtained from "test_get_metrics" method: {metrics_list}')
+    print(f'Result successfully obtained from "test_get_metrics" method')
+
+    entity_type_list = test_get_entity_types(env, token)
+    # print(f'Result successfully obtained from "test_get_entity_types" method: {entity_type_list}')
+    print(f'Result successfully obtained from "test_get_entity_types" method')
+
+    host_list = test_get_entity_type_host(env, token)
+    # print(f'Result successfully obtained from "test_get_entity_type_host" method: {host_list}')
+    print(f'Result successfully obtained from "test_get_entity_type_host" method')
+
+    endpoint = '/api/config/v1/autoTags'
+    payload = json.dumps({'name': 'TestAutoTag', 'rules': []})
+
+    r = test_post(env, token, endpoint, payload)
+    if r.status_code != 201:
+        print('Unexpected status code from "test_post" method')
+        print_response_details(r)
+        exit(1)
+
+    object_id = json.loads(r.text).get('id')
+    print(f'Posted {object_id} successfully with "test_post" method')
+
+    r = test_put(env, token, endpoint, object_id, payload)
+    if r.status_code != 204:
+        print('Unexpected status code from "test_put" method')
+        print_response_details(r)
+        exit(1)
+
+    print(f'Put {object_id} successfully with "test_put" method')
+
+    test_delete(env, token, endpoint, object_id)
+    print(f'Deleted {object_id} successfully with "test_delete" method')
+
+    endpoint = '/api/v2/metrics/ingest'
+    payload = 'com.dynatrace.automation.responseTime,language=python 1000'
+
+    r = test_post_plain_text(env, token, endpoint, payload)
+    if r.status_code != 202:
+        print('Unexpected status code {r.status_code} from "test_post_plain_text" method')
+        print_response_details(r)
+        exit(1)
+
+    print(f'Posted plain text object successfully')
+
+    print('End of sunny day tests...')
+    print('')
+
+    # print('Begin failure testing...')
+    # Must test one at a time since a failure generally results in an exit statement from dynatrace_api.py
+    # Test bad endpoint for get
+    # dynatrace_api.get(env, token, '/api/v2/metrics/BAD', None)
+    # Test bad endpoint for get object list
+    # test_get_object_list(env, token, '/api/config/v1/BAD')
+    # Test bad endpoint for delete
+    # test_delete(env, token, '/api/v2/bad/endpoint', 'BAD-OBJECT-ID')
+
+
+def print_response_details(r):
+    print(r)
+    print(r.status_code)
+    print(r.text)
+    print(r.reason)
+    print(r.url)
+    print(r.headers)
+    print(r.raw)
 
 
 if __name__ == '__main__':
     main()
-
