a3ed534777213d70545938f1b2bc6a4728ab6f10
dave.mauney <dave.mauney@dynatrace.com>
2023-12-04 12:25:37 -0500

    Converted deprecated method calls.

--- Reporting/DeploymentAPI/report_deployment_api_details.py
+++ Reporting/DeploymentAPI/report_deployment_api_details.py
@@ -1,3 +1,7 @@
+import json
+
+from requests.exceptions import HTTPError
+
 from Reuse import dynatrace_api
 from Reuse import environment
 from Reuse import report_writer
@@ -36,14 +40,13 @@ def append_report(report_name, headers, rows, tuple_lists):
 def process_connectivity_info(env, token):
     rows = []
     endpoint = '/api/v1/deployment/installer/agent/connectioninfo'
-    params = ''
-    connectivity_info_json_list = dynatrace_api.get(env, token, endpoint, params)
+    r = dynatrace_api.get_without_pagination(f'{env}{endpoint}', token)
+    connection_info_json = json.loads(r.text)
+    communication_endpoints = connection_info_json.get('communicationEndpoints')
 
     headers = ['Communication Endpoint']
-    for connectivity_info_json in connectivity_info_json_list:
-        communication_endpoints = connectivity_info_json.get('communicationEndpoints')
-        for communication_endpoint in communication_endpoints:
-            rows.append([communication_endpoint])
+    for communication_endpoint in communication_endpoints:
+        rows.append([communication_endpoint])
 
     return headers, sorted(rows)
 
@@ -51,10 +54,12 @@ def process_connectivity_info(env, token):
 def process_active_gate_endpoints(env, token):
     rows = []
     endpoint = '/api/v1/deployment/installer/agent/connectioninfo/endpoints'
-    active_gate_endpoints = dynatrace_api.get_plain_text_list(env, token, endpoint)
+    r = dynatrace_api.get_without_pagination(f'{env}{endpoint}', token)
+    active_gate_endpoints = r.text
     headers = (['ActiveGate Endpoint'])
     active_gate_endpoint_list = active_gate_endpoints.split(';')
     for active_gate_endpoint in active_gate_endpoint_list:
+        print(active_gate_endpoint)
         rows.append([active_gate_endpoint])
 
     return headers, sorted(rows)
@@ -63,19 +68,23 @@ def process_active_gate_endpoints(env, token):
 def process_versions(env, token):
     rows = []
     os_type_list = ['windows', 'unix', 'aix', 'solaris', 'zos']
-    installer_type_list = ['default', 'default-unattended', 'mainframe', 'paas', 'paas-sh']
+    # installer_type_list = ['default', 'default-unattended', 'mainframe', 'paas', 'paas-sh']
+    installer_type_list = ['default', 'default-unattended', 'paas', 'paas-sh']
 
     headers = (['OS Type', 'Installer Type', 'Available Version'])
     for os_type in os_type_list:
         for installer_type in installer_type_list:
             endpoint = f'/api/v1/deployment/installer/agent/versions/{os_type}/{installer_type}'
-            params = ''
-            agent_version_json_list = dynatrace_api.get(env, token, endpoint, params)
-            for agent_version_json in agent_version_json_list:
+            try:
+                r = dynatrace_api.get_without_pagination(f'{env}{endpoint}', token, handle_exceptions=False)
+                agent_version_json = json.loads(r.text)
                 available_versions = agent_version_json.get('availableVersions')
                 if available_versions:
                     for available_version in available_versions:
                         rows.append([os_type, installer_type, available_version])
+                        print(os_type, installer_type, available_version)
+            except HTTPError as e:
+                handle_http_exception(e, f'{env}{endpoint}')
 
     return headers, sorted(rows)
 
@@ -89,14 +98,23 @@ def process_latest_version(env, token):
     for os_type in os_type_list:
         for installer_type in installer_type_list:
             endpoint = f'/api/v1/deployment/installer/agent/{os_type}/{installer_type}/latest/metainfo'
-            params = ''
-            agent_version_json_list = dynatrace_api.get(env, token, endpoint, params)
-            for agent_version_json in agent_version_json_list:
+            try:
+                r = dynatrace_api.get_without_pagination(f'{env}{endpoint}', token, handle_exceptions=False)
+                agent_version_json = json.loads(r.text)
                 latest_version = agent_version_json.get('latestAgentVersion')
                 if latest_version:
                     rows.append([os_type, installer_type, latest_version])
+            except HTTPError as e:
+                handle_http_exception(e, f'{env}{endpoint}')
 
     return headers, sorted(rows)
+
+
+def handle_http_exception(e, url):
+    if "[404]" not in str(e.response):
+        print(f'HTTPError: URL: "{url}" Response: "{e.response}"')
+        # Want to log 404s?  Add else statment and uncomment:
+        # print(f'HTTPError (404 - Not Found): URL: {url}')
 
 
 def write_strings(string_list):
